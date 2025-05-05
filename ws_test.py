1414a3214d186b7734c8b2136f4e9df0fb64f679
Krzysztof Majerczyk-Marchewka <skanerooo5@gmail.com>
2025-05-05 19:42:06 +0200

    .

--- ws_test.py
+++ ws_test.py
@@ -1,9 +1,13 @@
 from websockets.sync.client import connect
 
-with connect("ws://localhost:7878/") as client:
+try:
+    with connect("ws://localhost:7878/") as client:
 
-    client.ping()
-    # client.send("set x 1", True)      # protocol is not yet fully implemented on server
-    # resp = client.recv()
-    # print(resp)
-    client.close_socket()
+        client.ping()
+        # client.send("set x 1", True)      # protocol is not yet fully implemented on server
+        # resp = client.recv()
+        # print(resp)
+        client.close_socket()
+
+except Exception as e:
+    print(e)
